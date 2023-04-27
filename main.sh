e2d7029abf7bbdbc9ed5be6b3461b40d57061c92
Sebastian009w <ia3266595@gmail.com>
2023-04-26 20:36:20 -0500

    permisos

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 # esta expresion ">>" no eliminara lo escrito anteriormente
+# permisos: chmod +x <namefile.sh>
 cat >> main.txt
 
+: 'este comentario de dos lineas
+puede llegar hacer util'
 # mas comentarios
---
+++ out.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cat << XYZ
+esto es un texto texto que se mostrara
+por consola
+XYZ
