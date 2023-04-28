8d5e03d998c2defb5277bb6ae77f81a0fbd7386b
Sebastian009w <ia3266595@gmail.com>
2023-04-28 16:55:41 -0500

    else caso contrario

--- conditional.sh
+++ conditional.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 age=10
+num=11
 
 if [ $age -eq 10 ]
 then
@@ -15,4 +16,13 @@ fi
 if [ $age -le 10 ]
 then
     echo "el nnumero es menor o igual a $age"
+fi    
+
+# else
+
+if [ $num -le 10 ]
+then
+    echo "el numero es menor que 10"
+else
+    echo "el numero es mayor que 10"    
 fi    
