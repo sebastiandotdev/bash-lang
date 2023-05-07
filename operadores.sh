e7cd2ca6a0de86bea568c029564f6ed73ee5c903
Sebastian009w <ia3266595@gmail.com>
2023-05-06 19:37:02 -0500

    operador ||

--- operadores.sh
+++ operadores.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
-age = 10
+age=30
+
 if [[ $age -gt 10  &&  $age -lt 40 ]]
 then
     echo "edad validad"
 else
     echo "edad invalida"
-fi    
+fi 
+
+if [[ $age -gt 10  ||  $age -lt 40 ]]
+then
+    echo "edad validad"
+else
+    echo "edad invalida"
+fi
