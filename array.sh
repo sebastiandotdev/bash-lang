4d9384c6196da69a65289defc27359273d05a7d5
Sebastian009w <ia3266595@gmail.com>
2023-05-12 21:30:38 -0500

    tomando el total de elemento en el array

--- array.sh
+++ array.sh
@@ -3,7 +3,10 @@
 opcion_one="rs"
 opcione_two="ts"
 
-name=( "sebastian" "mark" "james" "jhon" )
+name=( "sebastian" "mark" "james" "jhon" "alex" )
 
 echo ${name[@]}
 echo ${name[2]}
+
+echo ${!name[@]}
+echo ${#name[@]}
